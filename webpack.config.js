e99d0810691a08a3b4eee4e592ca23085518fd9a
Oscar Barajas <oscar@dragons.mx>
2021-02-26 10:53:12 -0500

    :art: [ADD] webpack babel

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const { resolve } = require('path');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        }
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 3006
+  }
+}
