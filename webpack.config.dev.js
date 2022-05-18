faa7852c5da5083fba1fa737e6bbb54cb03c67b7
Sergio Morgan <morgan.sergio@gmail.com>
2022-05-18 12:41:05 -0500

    fin de curso, prueba

---
+++ webpack.config.dev.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+    mode: 'development',
+    module: {
+        rules: [
+          {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader'
+            }
+          },
+          {
+            test: /\.html$/,
+            use: [
+                {loader: 'html-loader'}
+            ]
+          },
+          {
+            test: /\.s[ac]ss$/,
+            use: [
+                'style-loader',
+                // MiniCssExtractPlugin.loader,
+                'css-loader',
+                'sass-loader'
+            ],
+          },
+        ]
+    },
+
+    plugins : [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: './index.html'
+        }),
+
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        }),
+
+    ],
+
+    devServer: {
+        static: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 3006,
+    }
+}
